b5edeb01ce199668912ae7cd1a521c89f57e24e9
Mathias <mathiasrubert1@gmail.com>
2022-10-14 18:07:42 +0200

    Ajouts d'images + de code

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 require('tables')
 
 function love.load()
+  --Charger le curseur
   mouse.cursor = love.mouse.newCursor("images/cursor.png")
+  
+  --Charger le logo
+  logo = love.graphics.newImage("images/logo.png")
 end
 
 function love.draw()
+  --Dessiner le curseur
+  love.mouse.setCursor(mouse.cursor)
+  
+  --Dessiner le logo
+  love.graphics.draw(logo)
 end
 
 function love.update(dt)
